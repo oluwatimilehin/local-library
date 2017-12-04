af0d9add36644c247e8c2d55c4db146e23142cc0
Timi <oluwatimilehinadeniran@gmail.com>
2017-12-04 12:05:09 +0100

    Run migrations

--- catalog/models.py
+++ catalog/models.py
@@ -17,6 +17,20 @@ class Genre(models.Model):
         return self.name
 
 
+class Language(models.Model):
+    """
+    Model representing a Language (e.g. English, French, Japanese, etc.)
+    """
+    name = models.CharField(max_length=200,
+                            help_text="Enter a the book's natural language (e.g. English, French, Japanese etc.)")
+
+    def __str__(self):
+        """
+        String for representing the Model object (in Admin site etc.)
+        """
+        return self.name
+
+
 class Book(models.Model):
     """
        Model representing a book (but not a specific copy of a book).
