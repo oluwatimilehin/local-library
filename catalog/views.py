d97122e2c41a6f634d8698fc470a8f603e030d14
Timi <oluwatimilehinadeniran@gmail.com>
2018-01-10 23:03:47 +0100

    Set up book list and detail page

--- catalog/views.py
+++ catalog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 
 def index(request):
@@ -14,14 +15,20 @@ def index(request):
     # Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()  # The 'all()' is implied by default.
-
+    num_genres = Genre.objects.count()
+    num_books_with_keyword = Book.objects.filter(title__contains='it').count()
     # Render the HTML template index.html with the data in the context variable
     return render(
         request,
         'index.html',
         context={'num_books': num_books, 'num_instances': num_instances,
-                 'num_instances_available': num_instances_available, 'num_authors': num_authors},
+                 'num_instances_available': num_instances_available, 'num_authors': num_authors,
+                 'num_genres': num_genres, 'num_books_with_keyword': num_books_with_keyword}
     )
 
 
+class BookListView(generic.ListView):
+    model = Book
 
+class BookDetailView(generic.DetailView):
+    model = Book
